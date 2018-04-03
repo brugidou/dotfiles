41246aee2141151a15b3d932915dc91ff2738865
Maxime Brugidou <m.brugidou@criteo.com>
2018-04-03 17:07:16 +0200

    Add simple setup.sh script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt install xorg awesome chromium
+apt install laptop-mode-tools fdpowermon
+
+# Grand sudo access
+apt install sudo
+usermod -aG sudo m.brugidou
+
+# Get all submodules
+sudo -u m.brugidou git submodule update --init
+
+# Network
+apt install network-manager network-manager-vpnc network-manager-openconnect network-manager-gnome
+
+# Install vim plugins
+apt install vim-nox
+sudo -u m.brugidou vim +BundleInstall +q
+
+# Setup urxvt
+apt install rxvt-unicode xfonts-terminus
+update-alternatives --set x-terminal-emulator /usr/bin/urxvt
+
+# Setup ruby
+apt install ruby ruby-dev libyajl-dev build-essential libxml2-dev
+gem install bundler
