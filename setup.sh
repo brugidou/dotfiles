71b095ba292ea42f6993db7417b3496cbf122fa0
Maxime Brugidou <m.brugidou@criteo.com>
2019-11-25 22:51:37 +0100

    Add VSCode, .NET Core and IntelliJ

--- setup.sh
+++ setup.sh
@@ -33,3 +33,20 @@ apt install volumeicon-alsa
 
 # Update daily
 apt install unattended-upgrades
+
+# Install snaps
+apt install snapd
+# Visual Studio Code
+snap install code --classic
+# IntelliJ
+snap install intellij-idea-community --classic
+
+# Dotnet
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/9/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+apt update
+apt install dotnet-sdk-2.1=2.1.504-1
