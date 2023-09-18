15f97f978291ea192f1e57c2f13fe8684aa437e3
Maxime Brugidou <m.brugidou@criteo.com>
2023-09-18 13:36:51 +0200

    improve setup.sh for speed

--- setup.sh
+++ setup.sh
@@ -1,68 +1,6 @@
 #!/bin/bash
 
-apt update
-
-# firmwares (SOF firmware for sound card)
-apt install fwupd firmware-sof-signed
-fwupdmgr update
-
-apt install xorg lightdm awesome chromium arandr autorandr dex light-locker
-apt install tlp fdpowermon powertop
-
-# Grand sudo access
-apt install sudo
-usermod -aG sudo m.brugidou
-apt install krb5-user
-
-# Get all submodules
-sudo -u m.brugidou git submodule update --init
-
-# Network
-apt install network-manager network-manager-vpnc network-manager-openconnect network-manager-gnome network-manager-openconnect-gnome systemd-resolved
-# DNS resolver (useful for split DNS for VPN)
-systemctl enable systemd-resolved && systemctl start systemd-resolved
-
-# Install vim plugins
-apt install vim-nox
-sudo -u m.brugidou vim +BundleInstall +q
-
-# Setup urxvt
-apt install rxvt-unicode xfonts-terminus
-update-alternatives --set x-terminal-emulator /usr/bin/urxvt
-
-# Setup ruby
-apt install ruby ruby-dev libyajl-dev build-essential libxml2-dev
-gem install bundler
-
-# Screen backlight
-apt install xbacklight
-# Screenshots
-apt install flameshot
-
-# Bluetooth
-apt install blueman
-apt install pipewire-pulse libspa-0.2-bluetooth wireplumber
-# todo: check if need for systemctl --user enable wireplumber
-apt install volumeicon-alsa alsa-utils pavucontrol
-
-# Criteo printers
-apt install cups
-if ! lpstat -p | grep -q Criteo4th ; then
-  echo Adding printer for Criteo 4th floor...
-  lpadmin -p Criteo4th -m lsb/usr/cupsfilters/Generic-PDF_Printer-PDF.ppd -v socket://172.29.8.38:9100  -L 172.29.8.38
-fi
-
-# Update daily
-apt install unattended-upgrades
-
-# Install snaps
-apt install snapd
-# Node + NPM
-snap install node --classic
-# Visual Studio Code
-snap install code --classic
-# IntelliJ
-snap install intellij-idea-community --classic
+# APT Repositories
 
 # Dotnet
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
@@ -71,38 +9,119 @@ wget -q https://packages.microsoft.com/config/debian/9/prod.list
 mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+# Google Chrome
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub > /etc/apt/trusted.gpg.d/google-chrome.asc
+cat > /etc/apt/sources.list.d/google-chrome.list <<EOF
+deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
+EOF
+
+
+# Install JDK 8 from Adoptopenjdk and set it by default
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public > /etc/apt/trusted.gpg.d/adoptopenjdk.asc
+cat > /etc/apt/sources.list.d/jfrog.list <<EOF
+deb [arch=amd64] https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ stretch main
+EOF
+
+# Signal app
+wget -qO- https://updates.signal.org/desktop/apt/keys.asc > /etc/apt/trusted.gpg.d/signal.asc
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | tee /etc/apt/sources.list.d/signal-xenial.list
+
 apt update
-apt install dotnet-sdk-5.0
+
+installPkgs=(
+  # firmwares (SOF firmware for sound card)
+  fwupd firmware-sof-signed
+
+  xorg lightdm awesome chromium arandr autorandr dex light-locker
+  tlp fdpowermon powertop
+
+  sudo krb5-user
+
+  # Network
+  network-manager network-manager-vpnc network-manager-openconnect network-manager-gnome network-manager-openconnect-gnome systemd-resolved
+
+  vim-nox
+  rxvt-unicode xfonts-terminus
+  ruby ruby-dev libyajl-dev build-essential libxml2-dev
+
+  # Screen backlight
+  xbacklight
+  # Screenshots
+  flameshot
+
+  # Bluetooth
+  blueman
+  pipewire-pulse libspa-0.2-bluetooth wireplumber
+  # todo: check if need for systemctl --user enable wireplumber
+  volumeicon-alsa alsa-utils pavucontrol
+
+  cups
+
+  # Update daily
+  unattended-upgrades
+
+  # Install snaps
+  snapd
+
+  dotnet-sdk-5.0
+
+  google-chrome-stable
+
+  adoptopenjdk-8-hotspot
+
+  signal-desktop
+
+  docker.io
+)
+
+apt install ${installPkgs[@]}
+
+# Grand sudo access
+usermod -aG sudo m.brugidou
+
+# update firmwares
+fwupdmgr update
+
+# Get all submodules
+sudo -u m.brugidou git submodule update --init
+
+# DNS resolver (useful for split DNS for VPN)
+systemctl enable systemd-resolved && systemctl start systemd-resolved
+
+# Install vim plugins
+sudo -u m.brugidou vim +BundleInstall +q
+
+# Setup urxvt
+update-alternatives --set x-terminal-emulator /usr/bin/urxvt
+
+# Setup ruby
+gem install bundler
+
+# Criteo printers
+if ! lpstat -p | grep -q Criteo4th ; then
+  echo Adding printer for Criteo 4th floor...
+  lpadmin -p Criteo4th -m lsb/usr/cupsfilters/Generic-PDF_Printer-PDF.ppd -v socket://172.29.8.38:9100  -L 172.29.8.38
+fi
+
+# Node + NPM
+snap install node --classic
+# Visual Studio Code
+snap install code --classic
+# IntelliJ
+snap install intellij-idea-community --classic
 
 # Zoom
 wget https://zoom.us/client/latest/zoom_amd64.deb
 apt install ./zoom_amd64.deb
 rm -f ./zoom_amd64.deb
 
-wget -O- https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-cat > /etc/apt/sources.list.d/google-chrome.list <<EOF
-deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
-EOF
-apt update && apt install google-chrome
-
-# Install JDK 8 from Adoptopenjdk and set it by default
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
-cat > /etc/apt/sources.list.d/jfrog.list <<EOF
-deb [arch=amd64] https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ stretch main
-EOF
-apt update && apt install adoptopenjdk-8-hotspot
+# Set java 8 as default
 update-alternatives --set java /usr/lib/jvm/adoptopenjdk-8-hotspot-amd64/bin/java
 
-# Install signal app
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | tee /etc/apt/sources.list.d/signal-xenial.list
-apt update && apt install signal-desktop
-
 # Install docker
-apt install docker.io
 usermod -aG docker m.brugidou
 
 # Full upgrade and cleanup
-apt update
 apt dist-upgrade
 apt autoremove
